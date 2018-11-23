9ad3ecb0aefafbbc09b28ef2465a214bdad10698
JuanPablogomes17517 <45274200+JuanPablogomes17517@users.noreply.github.com>
2018-11-23 11:51:32 -0600

    Update PRUEBASERIAL.asm

--- PRUEBASERIAL.asm
+++ PRUEBASERIAL.asm
@@ -1,18 +1,16 @@
 #include "p16f887.inc"
 
 ; CONFIG1
-; __config 0xFFF4
- __CONFIG _CONFIG1, _FOSC_INTRC_NOCLKOUT & _WDTE_OFF & _PWRTE_OFF & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_OFF & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
+; __config 0xE0F4
+ __CONFIG _CONFIG1, _FOSC_INTRC_NOCLKOUT & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _CP_OFF & _CPD_OFF & _BOREN_OFF & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
 ; CONFIG2
 ; __config 0xFFFF
- __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF 
+ __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
 ;*******************************************************************************
 GPR_VAR		UDATA
-W_TEMP		RES 1      ; w register for context saving (ACCESS)
-STATUS_TEMP	RES 1      ; status used for context saving
-DELAY1		RES 1
-DELAY2		RES 1
-VALOR_ADC	RES 1
+CONT1 RES 1
+CONT2 RES 1
+W_TEMP RES 1 
 ;*******************************************************************************
 ; Reset Vector
 ;*******************************************************************************
